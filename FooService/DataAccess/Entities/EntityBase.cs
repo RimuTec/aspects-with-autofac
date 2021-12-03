ea0f55f90f1c9c4f5bd11c5e180e14fcfb5a5890
Manfred Lange <manfredmlange@gmail.com>
2021-12-03 06:28:00 +0000

    Fixed typo

--- FooService/DataAccess/Entities/EntityBase.cs
+++ FooService/DataAccess/Entities/EntityBase.cs
@@ -5,7 +5,7 @@ namespace FooService.DataAccess.Entities
    public class EntityBase : IHaveAuditInformation
    {
       public virtual Guid Oid { get; private set; }
-      // Note that NHibernate can set property 'Id' despite the setting being private [Manfred]
+      // Note that NHibernate can set property 'Id' despite the setter being private [Manfred]
       public virtual DateTime CreatedOn { get; set; }
       public virtual DateTime ModifiedOn { get; set; }
    }
